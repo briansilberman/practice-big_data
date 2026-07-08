d13af00c4ef9ea41a9355cb429cddac717466738
Brian Silberman <brian.silberman78@gmail.com>
2026-07-08 15:54:42 -0400

    Adding initial practice assignment file and supplementary files.

---
+++ song_count.py
@@ -0,0 +1,22 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class MRCountSongs(MRJob):
+    def steps(self):
+        return [MRStep(mapper = self.mapper), MRStep(reducer = self.reducer)]
+    
+    #Each line will be read as a key, value:
+    #Each line has no key, so we ignore it with "_":
+    def mapper(self, _, song):
+        #Each line is a tuple: 
+        yield (song, 1)
+
+    #Combine all tuples with the same key:
+    def reducer(self, key, values):
+        #Key is the song name.
+        #Sum up values in the tuple to get total song plays:
+        yield (key, sum(values))
+        
+#Run this if I call it via the terminal:    
+if __name__ == "__main__":
+    MRCountSongs.run()
